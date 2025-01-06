a310ff11368533db7489c0b5fd6e3793313c00c1
Hummel Vendel <vendel.hummel@gmail.com>
2025-01-06 20:36:25 +0100

    Asztali aplikációhoz szükséges adatbázis műveletek végrehajtó fügvények létrehozva a db.js fájlban.

--- db.js
+++ db.js
@@ -21,10 +21,52 @@ connection.connect((err) => {
     console.log('Connected to the MySQL server.');
 });
 
-
-export async function penztarok()
+// Desktop Endpoints
+export async function DPenztarok()
 {
     let sql = 'SELECT DISTINCT szamla.spenztar FROM szamla WHERE 1';
     const [result] = await connection.execute(sql);
     return result;
 }
+
+export async function DBelepes(aazon) {
+    let sql = 'SELECT alkalmazott.agepjog FROM alkalmazott WHERE aazon = ?';
+    const [result] = await connection.execute(sql, [aazon]);
+    return result;
+}
+
+export async function DAlkalmazott(aazon) {
+    let sql = 'SELECT alkalmazott.anev, alkalmazott.amunka, alkalmazott.aszul, alkalmazott.abelepes FROM alkalmazott WHERE aazon = ?';
+    const [result] = await connection.execute(sql, [aazon]);
+    return result;
+}
+
+export async function DTermekLista() {
+    let sql = 'SELECT termek.tazon, termek.tnev, termek.tkategoria FROM termek WHERE 1';
+    const [result] = await connection.execute(sql);
+    return result;
+}
+
+export async function DTermek(tazon) {
+    let sql = 'SELECT termek.tar, termek.tmennyiseg, termek.tmennyisegiegyseg, termek.tkoros FROM termek WHERE tazon = ?';
+    const [result] = await connection.execute(sql, [tazon]);
+    return result;
+}
+
+export async function DUjSzamla(spenztar, selado, sfizetesimod) {
+    let sql = 'INSERT INTO szamla (szamla.spenztar, szamla.selado, szamla.sfizetesimod) VALUES (?, ?, ?)';
+    const [result] = await connection.execute(sql, [spenztar, selado, sfizetesimod]);
+    return result;
+}
+
+export async function DUjTetel(sazon, tazon, mennyiseg) {
+    let sql = 'INSERT INTO tetel (tetel.sazon, tetel.tazon, tetel.mennyiseg) VALUES (?, ?, ?)';
+    const [result] = await connection.execute(sql, [sazon, tazon, mennyiseg]);
+    return result;
+}
+//
+
+// Web Endpoints
+
+
+//

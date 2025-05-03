5057b4b1a7d4c6325ad34b9a488067a121c5082a
Róbert Hunka <hunkarobert60@gmail.com>
2025-05-03 16:41:18 +0200

    Duplicated line of code fixef

--- backend/db.js
+++ backend/db.js
@@ -14,13 +14,6 @@ try {
         database: process.env.DB_NAME || "db_nyilvantartas",
     });
 
-const connection = await mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306,
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME || 'db_nyilvantartas',
-});
     
 } catch (error) {
     console.error("Failed to connect to database:", error);

947aa848a2f3b1cf7d5d846096fbff9cc158e5d3
hunka <hunkarobert60@gmail.com>
2025-05-04 16:20:48 +0200

    text added to frontend, fixed duplication issue, developers' pictures added

--- backend/db.js
+++ backend/db.js
@@ -8,20 +8,12 @@ let connection;
 try {
     connection = await mysql.createConnection({
         host: process.env.DB_HOST || "localhost",
-        port: process.env.DB_PORT || 3307,
+        port: process.env.DB_PORT || 3306,
         user: process.env.DB_USER || "root",
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME || "db_nyilvantartas",
     });
 
-const connection = await mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306,
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME || 'db_nyilvantartas',
-});
-    
 } catch (error) {
     console.error("Failed to connect to database:", error);
     throw error;
